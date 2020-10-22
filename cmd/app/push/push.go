10072b83b452a22cb73b0f5317d6ce9885064053
Rex T Wu <rex_t_wu@trendmicro.com>
2020-10-22 22:50:51 +0800

    Add compression feature

--- cmd/app/push/push.go
+++ cmd/app/push/push.go
@@ -15,6 +15,8 @@ var (
 	destination string
 	awsBucket   string
 	awsRegion   string
+	zipFile     bool
+	rename      string
 )
 
 func New() app.AppService {
@@ -24,6 +26,8 @@ func New() app.AppService {
 func (a *App) Init(ctx *cobra.Command) error {
 	ctx.Flags().StringVarP(&awsRegion, "region", "", "", "set s3 bucket region")
 	ctx.Flags().StringVarP(&awsBucket, "bucket", "", "", "set s3 bucket name")
+	ctx.Flags().StringVarP(&rename, "rename", "", "", "set the zip file name")
+	ctx.Flags().BoolVarP(&zipFile, "zip", "", false, "type ture or false. if true, and the source arguments is a correct folder or file path, it will compress to a zip file and upload")
 	return nil
 }
 
@@ -31,12 +35,25 @@ func (a *App) Start(ctx *cobra.Command, args []string) error {
 	source := args[0]
 	destination := args[1]
 
+	var path string
+	var err error
+	if zipFile {
+		path = getZipName(source)
+		err = file.Zip(source, path)
+	} else {
+		path = source
+	}
+
+	if err != nil {
+		return err
+	}
+
 	s3Svr, err := aws_tool.New(awsRegion)
 	if err != nil {
 		return err
 	}
 
-	buffer, err := file.ReadFileBuffer(source)
+	buffer, err := file.ReadFileBuffer(path)
 	if err != nil {
 		return err
 	}
@@ -45,9 +62,31 @@ func (a *App) Start(ctx *cobra.Command, args []string) error {
 		return err
 	}
 
+	if zipFile {
+		err = file.RemoveFile(path)
+	}
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (a *App) Close(ctx *cobra.Command) error {
 	return nil
+}
+
+func getZipName(source string) string {
+	var zipName string
+	if len(rename) != 0 {
+		zipName = rename
+	}
+
+	if file.IsDir(source) {
+		zipName = file.GetFolderName(source)
+	} else {
+		zipName = file.GetFileName(source)
+	}
+	return zipName + ".zip"
 }
--- cmd/app/utils/file/file.go
+++ cmd/app/utils/file/file.go
@@ -1,6 +1,33 @@
 package file
 
-import "os"
+import (
+	"os"
+	"path/filepath"
+)
+
+func GetFileName(path string) string {
+	return filepath.Base(path)
+}
+
+func GetFolderName(path string) string {
+	dir := filepath.Dir(path)
+	if dir == "." {
+		return path
+	}
+	return dir
+}
+
+func IsDir(path string) bool {
+	info, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return info.IsDir()
+}
+
+func RemoveFile(path string) error {
+	return os.Remove(path)
+}
 
 func ReadFileBuffer(path string) ([]byte, error) {
 	file, err := os.Open(path)
